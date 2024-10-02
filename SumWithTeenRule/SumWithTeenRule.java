2e40beb7d195f0ca97115047e15096473c90a2d3
isra <imorasbi@gmail.com>
2024-10-02 10:13:55 +0400

    first commit

---
+++ SumWithTeenRule/SumWithTeenRule.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class SumWithTeenRule {
+    public static void main(String[] args) {
+        //take input from user
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int first = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int second = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int third = sc.nextInt();
+        System.out.println("Ths sum: " +noTeenSum( first,second,third));
+
+    }
+    public static int fixTeen(int n) {
+        //check that the number is between the range 13 nd 19
+       if (n>=13 && n<=19) {
+           //if the number is 15 or 16 then keep it as it is
+           if(n==15 || n==16){
+               return n;
+           }
+           //otherwise change the number to 0
+           else{
+               n=0;
+           }
+       }
+       //return the number
+       return n;
+    }
+
+    public static int noTeenSum(int num1, int num2, int num3) {
+        return fixTeen(num1)+fixTeen(num2)+fixTeen(num3);
+    }
+
+}
